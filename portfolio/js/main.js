3a5b529426f8f3af3eba940d61482f8ef7e962f7
Adam Kwoka <adam.kwoka99@gmail.com>
2021-07-17 12:22:33 +0200

    Add final version

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,11 +1,13 @@
 // Add click to menu burger
 const burger = document.querySelector('.burger');
 const btn = document.querySelector('.menu-btn')
-const menu = document.querySelector('.mobile');
+const menu = document.querySelector('.main-nav');
+const body = document.querySelector('.wrap-body')
 
-burger.addEventListener('click', function () {
+btn.addEventListener('click', function () {
   btn.classList.toggle("show");
   menu.classList.toggle("show");
+  body.classList.toggle("move");
 })
 
 // Add scroll animation
@@ -20,6 +22,7 @@ $('nav ul li a').on('click', function () {
   $('body, html').animate({
     scrollTop: $("#" + $(this).attr('class')).offset().top
   }, 500)
-  $('.mobile').toggleClass('show');
+  $('.main-nav').toggleClass('show');
   $('.menu-btn').toggleClass('show');
+  body.classList.toggle("move");
 })
